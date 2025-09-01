b9d9a7d6bfa672df7f2785df478c00d325a71949
DS <danny.santos41618@gmail.com>
2025-09-01 18:36:10 -0400

    Completed Python3 Data Types lesson

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+favorite_cars = [
+    {
+        "make": "Tesla",
+        "model": "Model Y",
+        "year": 2025,
+        "features": ["Electric", "Autopilot", "Long Range"],
+        "is_electric": True
+    },
+    {
+        "make": "Mercedes-Benz",
+        "model": "300 Sedan",
+        "year": 1955,
+        "features": ["Classic", "V8 Engine", "Luxury Interior"],
+        "is_electric": False
+    }
+]
