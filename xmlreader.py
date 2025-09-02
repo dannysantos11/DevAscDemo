e958f3d6003cb94f0e56a7211b1c3b76ea25ae9e
DS <danny.santos41618@gmail.com>
2025-09-02 19:30:26 -0400

    parsing xml to python

---
+++ xmlreader.py
@@ -0,0 +1,10 @@
+import xmltodict
+
+with open('r1.xml') as file:
+    xml_data = file.read()
+
+data = xmltodict.parse(xml_data)
+
+print(data)
+
+print(data['router']['interfaces']['interface'][0]['ip'])
